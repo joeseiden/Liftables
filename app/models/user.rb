77a1596ff5020137d3d73491caffde1aa75b7975
Joe Seiden <joe.seiden214@gmail.com>
2017-03-14 14:19:28 -0700

    Create static page and add users controller/view

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   attr_reader :password
 
   def self.find_by_credentials(username, password)
-    user = User.find_by_username(username)
+    user = self.class.find_by_username(username)
     return user if user && user.is_password?(password)
     nil
   end
@@ -18,7 +18,7 @@ class User < ApplicationRecord
     SecureRandom::urlsafe_base64(16)
   end
 
-  def self.generate_password_digest
+  def self.generate_password_digest(password)
     BCrypt::Password.create(password)
   end
 
@@ -28,11 +28,11 @@ class User < ApplicationRecord
 
   def password=(password)
     @password = password
-    self.password_digest = BCrypt::Password.create(password)
+    self.password_digest = self.class.generate_password_digest(password)
   end
 
   def reset_session_token!
-    self.session_token = User.generate_session_token
+    self.session_token = self.class.generate_session_token
     self.save!
     self.session_token
   end
@@ -40,8 +40,8 @@ class User < ApplicationRecord
   private
 
   def ensure_session_token
-    self.session_token ||= User.generate_session_token
-  end 
+    self.session_token ||= self.class.generate_session_token
+  end
 
   def ensure_session_token_uniqueness
     while User.find_by(session_token: self.session_token)
