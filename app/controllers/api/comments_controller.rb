8c50c335084a67e2939ba339bef9a39f98929042
Joe Seiden <joe.seiden214@gmail.com>
2017-03-23 10:50:50 -0700

    Create comments controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,36 @@
+class Api::CommentsController < ApplicationController
+
+  def index
+    @comments = Article.includes(:comments).find(params[:article_id]).comments
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render 'api/comments/show'
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    render 'api/comments/show'
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      render 'api/comments/show'
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :article_id)
+  end
+end
