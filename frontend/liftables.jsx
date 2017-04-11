701f76c1eeddd64a1c093e95f3baeb8c670da6dd
joeseiden <joe.seiden214@gmail.com>
2017-04-11 10:14:40 -0700

    Add script to keep dyno awake

--- frontend/liftables.jsx
+++ frontend/liftables.jsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 import { login, logout } from './actions/session_actions';
+const http = require('http');
 
 const windowFunctions = { login, logout };
 
 Object.assign(window, windowFunctions);
+
+setInterval(() => {
+  http.get("http://liftables.herokuapp.com");
+}, 900000);
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
