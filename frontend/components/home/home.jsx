d68d6eaca868796d3b583afb1c1e6a54c4fb05ca
joeseiden <joe.seiden214@gmail.com>
2017-04-17 12:23:09 -0700

    Change font and restyle header, front page, articles index, search bar and article view

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -2,21 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {hashHistory, Link} from 'react-router';
 import SearchBar from '../search/search_bar';
+import ArticlesIndexContainer from '../articles/articles_index_container';
 
 class Home extends React.Component {
   render (){
 
     return (
-      <div className='splash-container'>
-        <div className='welcome'>
-          <h1 id='welcome-message'>Welcome to Liftables!</h1>
-          <p>
-            Create and share instructive articles about fitness and lifting weights!
-          </p>
-        </div>
-        <div className='splash-nav'>
-          <span>See <Link to='articles'>all articles</Link> or enter a term to search below...</span>
-          <SearchBar />
+      <div>
+        <div className='splash-container'>
+          <div className='welcome'>
+            <h1 id='welcome-message'>Welcome to liftables!</h1>
+            <p>
+              Create and share instructive articles about fitness and lifting weights!
+            </p>
+          </div>
+          <div className='splash-nav'>
+            <span>See <Link to='articles'>all articles</Link> or enter a term to search below...</span>
+            <SearchBar />
+          </div>
         </div>
       </div>
     );
