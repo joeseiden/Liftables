7e5caa300a11c759ec3750288fdae41e5f35d1d9
joeseiden <joe.seiden214@gmail.com>
2017-05-08 10:59:44 -0700

    Create user profile container

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -3,7 +3,7 @@ import * as UserAPIUtil from '../util/user_api_util';
 export const RECEIVE_USER = "RECEIVE_USER";
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
 
-export const fetchUser = userId => dispatch => (
+export const requestUser = userId => dispatch => (
   UserAPIUtil.fetchUser(userId)
     .then(user => dispatch(receiveUser(user)),
       err => dispatch(receiveErrors(err.responseJSON)))
--- frontend/components/user_profile/user_profile_container.js
+++ frontend/components/user_profile/user_profile_container.js
@@ -3,3 +3,17 @@ import UserProfile from './user_profile';
 import {
   requestUser
 } from '../../actions/user_actions';
+
+const mapStateToProps = (state, ownProps) => ({
+  user: state.user,
+  currentUser: state.session.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  requestUser: userId => dispatch(requestUser(userId))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserProfile);
